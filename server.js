2113f492ff8ce24e526984865f923d109ea0dc98
giuseppemorcillo <beppebeppe18@gmail.com>
2025-05-10 01:58:17 +0200

    feat: create server.js  dove importero' i miei file necessari

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const moviesRouter = require('./routes/moviesRouter');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use('/api/movies', moviesRouter);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
