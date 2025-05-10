c373657e137fc49ec7e01684f234a92cadf880cd
giuseppemorcillo <beppebeppe18@gmail.com>
2025-05-10 02:23:56 +0200

    feat: importato i middlewares nel mio file server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express    = require('express');
 const moviesRouter = require('./routes/moviesRouter');
-
+const notFound   = require('./middlewares/notFound');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
