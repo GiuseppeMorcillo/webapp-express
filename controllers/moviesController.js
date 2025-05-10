4b8e54a036de803ece346570eac27636805aa28c
giuseppemorcillo <beppebeppe18@gmail.com>
2025-05-10 03:04:45 +0200

    feat: ora il campo image mi restituisce l'immagine con il proprio url

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,50 +2,54 @@ const connection = require('../db');
 
 // Restituisce tutti i film con callback-style
 function index(req, res) {
-  const sql = `
+    const sql = `
     SELECT m.*, AVG(r.vote) AS average_rating
     FROM movies m
     LEFT JOIN reviews r ON m.id = r.movie_id
     GROUP BY m.id
   `;
 
-  connection.query(sql, (err, results) => {
-    if (err) {
-      console.error('Errore nella query INDEX:', err);
-      return res.status(500).json({ error: 'Errore interno al server' });
-    }
-    res.status(200).json(results);
-  });
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error('Errore nella query INDEX:', err);
+            return res.status(500).json({ error: 'Errore interno al server' });
+        }
+        res.json(results.map(result => ({
+            ...result,
+            image: process.env.PUBLIC_PATH + "movies_cover/" + result.title.toLowerCase() + ".jpg"
+        })))
+      
+    });
 }
 
 // Restituisce un singolo film con le sue recensioni
 function show(req, res) {
-  const filmId = req.params.id;
-  const sqlFilm = 'SELECT * FROM movies WHERE id = ?';
+    const filmId = req.params.id;
+    const sqlFilm = 'SELECT * FROM movies WHERE id = ?';
 
-  connection.query(sqlFilm, [filmId], (err, films) => {
-    if (err) {
-      console.error('Errore nella query SHOW film:', err);
-      return res.status(500).json({ error: 'Errore interno al server' });
-    }
-    if (films.length === 0) {
-      return res.status(404).json({ error: 'Film non trovato' });
-    }
+    connection.query(sqlFilm, [filmId], (err, films) => {
+        if (err) {
+            console.error('Errore nella query SHOW film:', err);
+            return res.status(500).json({ error: 'Errore interno al server' });
+        }
+        if (films.length === 0) {
+            return res.status(404).json({ error: 'Film non trovato' });
+        }
 
-    const movie = films[0];
-    const sqlReviews = 'SELECT * FROM reviews WHERE movie_id = ?';
+        const movie = films[0];
+        const sqlReviews = 'SELECT * FROM reviews WHERE movie_id = ?';
 
-    connection.query(sqlReviews, [filmId], (err2, reviews) => {
-      if (err2) {
-        console.error('Errore nella query SHOW reviews:', err2);
-        return res.status(500).json({ error: 'Errore interno al server' });
-      }
-      res.status(200).json({ ...movie, reviews });
+        connection.query(sqlReviews, [filmId], (err2, reviews) => {
+            if (err2) {
+                console.error('Errore nella query SHOW reviews:', err2);
+                return res.status(500).json({ error: 'Errore interno al server' });
+            }
+            res.status(200).json({ ...movie, reviews });
+        });
     });
-  });
 }
 
 module.exports = {
-  index,
-  show,
+    index,
+    show,
 };
