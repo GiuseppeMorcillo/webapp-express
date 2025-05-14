118421eecf6d1e6d8de7c8f1b74ead2b1a4defd0
giuseppemorcillo <beppebeppe18@gmail.com>
2025-05-14 14:05:04 +0200

    feat: aggiungot express.json per poter leggere i file json

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
+app.use(express.json());
 app.use('/api/movies', moviesRouter);
 
 app.use(notFound);
